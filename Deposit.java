54b9ec034ec9f24768a0991e9124ba2af4ae7a8c
Faheem Khan <111926858+Fame6435@users.noreply.github.com>
2024-01-30 14:22:35 -0500

    Create Deposit.java

---
+++ Deposit.java
@@ -0,0 +1,17 @@
+package BankManagementSystem;
+
+import java.util.Scanner;
+
+public class Deposit {
+	
+	public Deposit(Scanner scanner, Account acc) {
+		
+		System.out.println("Enter amount:");
+		double amount = scanner.nextDouble();
+		acc.setBalance(acc.getBalance() + amount);
+		System.out.println("Operation done successfully");
+		
+		
+	}
+
+}
